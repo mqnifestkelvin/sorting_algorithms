ce47379af3628885f81a006df302d57c2036cf40
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-01-22 22:06:13 +0100

    modified

--- sort.h
+++ sort.h
@@ -25,8 +25,9 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
-void swap(listint_t *a, listint_t *b);
+/*void swap(listint_t *a, listint_t *b);*/
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
