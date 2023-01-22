fccb982b9b8ab202fff2957d98f7867d02cbf073
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-01-22 19:31:33 +0100

    modified

--- sort.h
+++ sort.h
@@ -22,7 +22,10 @@ typedef struct listint_s
 
 /*print.c*/
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+void swap(listint_t *a, listint_t *b);
+void insertion_sort_list(listint_t **list);
 
 #endif
