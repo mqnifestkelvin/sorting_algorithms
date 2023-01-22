cd38306dd7a6c1f72634f0da4e13dbf9cdba820a
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-01-22 17:31:54 +0100

    sorting algorithms header files

---
+++ sort.h
@@ -0,0 +1,24 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * struct_listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void bubble_sort(int *array, size_t size);
+
+
